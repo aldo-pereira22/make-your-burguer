9446a987b5ee034a7a839631029eed9bd3c9da92
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-20 18:29:44 -0300

    Estilizando a página home

--- front-end/make_your_burguer/src/App.vue
+++ front-end/make_your_burguer/src/App.vue
@@ -27,24 +27,20 @@
 </script>
 
 <style>
-  *{
+  * {
     font-family: Helvetica;
     padding: 0;
     margin: 0;
     box-sizing: border-box;
   }
-
-  .main-container{
+  .main-container {
     margin: 50px;
     min-height: 250px;
   }
-
   h1 {
     text-align: center;
     font-size: 42px;
     margin-bottom: 30px;
     color: #222;
   }
-
-
 </style>
