8ff77ab4d47ced3b1ed00997cbe7ce4c5a97dc1a
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-20 19:03:17 -0300

    Formulário de escolha do burger

--- front-end/make_your_burguer/src/views/Home.vue
+++ front-end/make_your_burguer/src/views/Home.vue
@@ -3,18 +3,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu Burger:</h1>
-    <p> Formulário </p>
+  <BurgerForm />
   </div>
 </template>
 
 <script>
 
   import Banner from "../components/Banner.vue"
+  import BurgerForm from "../components/BurgerForm.vue"
 
   export default{
     name:"Home",
     components:{
-      Banner
+      Banner,
+      BurgerForm
     }
   }
 </script>
