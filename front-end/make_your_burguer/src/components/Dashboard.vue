880e03adcedf07550174d16aa37d014ec7a217e5
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-23 19:11:15 -0300

    Estilo da página Dashboard

--- front-end/make_your_burguer/src/components/Dashboard.vue
+++ front-end/make_your_burguer/src/components/Dashboard.vue
@@ -11,6 +11,7 @@
             </div>
 
             <div id="burger-table-rows">
+                
                 <div class="burger-table-row">
                     <div class="order-number">1</div>
                     <div>Joõa</div>
@@ -25,11 +26,35 @@
 
                     <div>
                         <select name="status" class="status">
-                            <option value="">Selecione o status do hamburger</option>
+                            <option value="">Selecione</option>
                         </select>
                         <button class="delete-btn">Cancelar</button>
                     </div>
                 </div>
+
+
+                <div class="burger-table-row">
+                    <div class="order-number">1</div>
+                    <div>Joõa</div>
+                    <div>Pão de trigo</div>
+                    <div>Maminha</div>
+                    <div>
+                        <ul>
+                            <li>Salame</li>
+                            <li>Tomate</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <select name="status" class="status">
+                            <option value="">Selecione</option>
+
+                        </select>
+                        <button class="delete-btn">Cancelar</button>
+                    </div>
+                </div>
+
+                
             </div>
         </div>
     </div>
@@ -55,5 +80,51 @@
     .burger-table-row{
         display: flex;
         flex-wrap: wrap;
+    }
+
+    #burger-table-heading{
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+
+    #burger-table-heading div,
+    .burger-table-row div {
+        width: 19%;
+
+    }
+
+    .burger-table-row{
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #CCC;
+    }
+
+    #burger-table-heading .order-id,
+    .burger-table-row .order-number{
+        width: 5%;
+    }
+
+    select{
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .delete-btn{
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover{
+
+        background-color: transparent;
+        color: #222;
     }
 </style>
