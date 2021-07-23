0bff3b4583fe91b7850592d8c64c300c87b9bcbe
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-23 18:59:28 -0300

    Construção da dashboard

---
+++ front-end/make_your_burguer/src/components/Dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="burger-table">
+        <div>
+            <div id="burger-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+
+            <div id="burger-table-rows">
+                <div class="burger-table-row">
+                    <div class="order-number">1</div>
+                    <div>Joõa</div>
+                    <div>Pão de trigo</div>
+                    <div>Maminha</div>
+                    <div>
+                        <ul>
+                            <li>Salame</li>
+                            <li>Tomate</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <select name="status" class="status">
+                            <option value="">Selecione o status do hamburger</option>
+                        </select>
+                        <button class="delete-btn">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        name:"Dashboard"   
+    }
+</script>
+
+<style scoped>
+
+    #burger-table{
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #burger-table-heading,
+    #burger-table-rows,
+    .burger-table-row{
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
